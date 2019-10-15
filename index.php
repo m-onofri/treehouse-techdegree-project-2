2772fabbbaef9f19af9c92a88827b1c8c9c1d230
m-onofri <manuele.onofri@gmail.com>
2019-10-15 14:16:31 +0200

    Add comments to index.php

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
     session_start();
     include('inc/questions.php');
 
+    //Collect and shuffle the questions
     $total = count($questions);
     if (empty($_SESSION['questions'])) {
         shuffle($questions);
         $_SESSION['questions'] = $questions;
     }
 
+    //Keep track of the current question
     if (empty($_SESSION['index']) && empty($_POST["id"])) {
         $_SESSION['index'] = 0;
     } else {
         $_SESSION['index'] = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
     }
 
+    //Random answer option
     $answers_array = [
         $_SESSION['questions'][$_SESSION['index']]['correctAnswer'],
         $_SESSION['questions'][$_SESSION['index']]['firstIncorrectAnswer'],
@@ -21,12 +24,14 @@
     ];
     shuffle($answers_array);
 
+    //Keep track of the user's answers
     if (isset($_POST['answer'])) {
         if (!isset($_SESSION['result'])) {
             $_SESSION['result'] = ['right' => 0, 'wrong' => 0];
         }
         $answer = filter_input(INPUT_POST, 'answer', FILTER_SANITIZE_STRING);
 
+        //Set toast
         if ($answer == $_SESSION['questions'][$_SESSION['index']]['correctAnswer']) {
             $toast = "Great, your answer is right!";
             $_SESSION['result']['right']++;
@@ -36,12 +41,12 @@
         }
     }
 
+    //Manage quiz result
     if ($_SESSION["index"] == $total) {
         $right_answers = $_SESSION['result']['right'];
         $wrong_answers = $_SESSION['result']['wrong'];
         session_destroy();
-    }
-    
+    }   
 ?>
 
 <!DOCTYPE html>
@@ -55,7 +60,10 @@
 </head>
 <body>
     <div class="container">
+
         <?php if (!isset($_POST["answer"]) && ($_SESSION["index"] < $total)) { ?>
+
+            <!-- Display the question and the answer options -->
             <div id="quiz-box">
                 <p class="breadcrumbs">Question <?php echo $_SESSION["index"] + 1; ?> of <?php echo $total; ?></p>
                 <p class="quiz">What is <?php echo $_SESSION['questions'][$_SESSION["index"]]["leftAdder"]; ?> + <?php echo $_SESSION['questions'][$_SESSION["index"]]["rightAdder"]; ?>?</p>
@@ -66,7 +74,10 @@
                     <?php } ?>
                 </form>
             </div>
+
         <?php } elseif (isset($_POST["answer"]) && ($_SESSION["index"] < $total)) { ?>
+
+            <!-- Display if the use get the right answer -->
             <div id="quiz-box">
                 <p class="toast"><?php echo $toast; ?></p>
                 <form action="index.php"  method="post">
@@ -74,14 +85,19 @@
                     <input type="submit" class="btn" value="Next Question" />
                 </form>
             </div>
+
         <?php } else { ?>
+        
+            <!-- Display the final score -->
             <div id="quiz-box">
                 <p class="result">You gave <?php echo $right_answers; ?> right answers and <?php echo $wrong_answers; ?> wrong answers</p>
                 <form action="index.php"  method="post">
                     <input type="submit" class="btn" value="New Quiz" />
                 </form>
             </div>
+
         <?php } ?>
+
     </div>
 </body>
 </html>
